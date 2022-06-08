d8902775402ca38b932757c6cbaa3bc5bb253bd8
parresh77 <parresh77@gmail.com>
2022-06-09 04:26:12 +0530

    :children_crossing: :art: Lots of Code Formatting and UI Updates
    * Formatted code in HTML & JS file.
    * Fixed problems with modal.
    * Replaced dummy words with real words.
    * Improved the stacking of tuition medium cards to increase responsiveness.
    * Replaced links that looked like buttons with actual buttons.
    * Added URLs to mentors' social media handle icons.

--- mapbox.js
+++ mapbox.js
@@ -1,8 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFyZXNoLXdhZGh3YW5pIiwiYSI6ImNrcXozZW80ZjB4Y2wyd254ejU4NWc3c2oifQ.QuSmYjuj4HRk84zJ77cRBg';
 
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
-center: [-73.9855, 40.7580],
-zoom: 18,
-});
+var map = new mapboxgl.Map
+(
+	{
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [-73.9855, 40.7580],
+		zoom: 18,
+	}
+);
